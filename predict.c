581e7c0d9741923f5f558d8ef7eb652899a3c5db
Ben Gaudiosi <bgaudiosi531@gmail.com>
2018-12-12 11:51:56 -0500

    found more unnecessary code

--- predict.c
+++ predict.c
@@ -22,18 +22,10 @@
 #include <math.h>
 #include <time.h>
 #include <sys/time.h>
-//#include <curses.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <pthread.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
 
 #include "predict.h"
 
