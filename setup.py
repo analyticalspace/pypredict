eddea6882a7b1b0019290999eebfc276747f9958
Ben Gaudiosi <bgaudiosi531@gmail.com>
2019-01-08 10:38:23 -0500

    changed setup url

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup, Extension
 setup(
     name='pypredict',
-    version='1.2',
+    version='2.0',
     author="Jesse Trutna",
     author_email="jesse@spire.com",
-    url="https://github.com/nsat/pypredict",
+    url="https://github.com/analyticalspace/pypredict",
     py_modules=['predict'],
     ext_modules=[Extension('cpredict', ['predict.c', 'pypredict.c'])]
     )
